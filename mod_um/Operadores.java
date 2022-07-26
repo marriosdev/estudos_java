8a3f631068784ae6ac9efd0bb6e43ff29b9b4daa
Edmário Oliveira <49793161+edmariooliver@users.noreply.github.com>
2022-07-25 22:30:02 -0300

    Update Operadores.java

--- mod_um/Operadores.java
+++ mod_um/Operadores.java
@@ -2,9 +2,6 @@ package mod_um;
 
 public class Operadores {
     public static void main(String[] args) {
-        // TIPOS DE OPERADORES
-        // LOGICOS
-        // ARITMETICOS
-        // COMPARAÇÃO
+
     }
 }
